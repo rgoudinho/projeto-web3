a20db55ba916e42a610cc653e31375ab9c4f36b2
rgoudinho <ronaldos@alunos.utfpr.edu.br>
2019-05-29 12:17:28 -0300

    Commit inicial

--- mvc/Visao/templates/index.php
+++ mvc/Visao/templates/index.php
@@ -4,10 +4,8 @@
 <head>
     <title><?= APLICACAO_NOME ?></title>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?= URL_CSS . 'bootstrap.css' ?>">
-
+    <link rel="stylesheet" href="<?= URL_CSS . 'cadastro-login.css' ?>">
 </head>
 
 <body>
